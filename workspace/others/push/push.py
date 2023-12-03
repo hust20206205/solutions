2bdd7f27d011adedf138b8e4c8e87d17303087a4
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-03 10:54:02 +0700

    2023_12_03_10_54_02_181522

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -21,46 +21,46 @@ import os
 print(os.getcwd())
 os.chdir("../../../document/latex")
 print(os.getcwd())
-latex_folder = os.getcwd()
-file_paths = glob.glob(os.path.join(latex_folder, f'**/*.tex'), recursive=True)
-for file_path in file_paths:
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.read()
+# latex_folder = os.getcwd()
+# file_paths = glob.glob(os.path.join(latex_folder, f'**/*.tex'), recursive=True)
+# for file_path in file_paths:
+#     with open(file_path, 'r', encoding="utf-8") as file:
+#         content = file.read()
 
-    content = content.replace('\\\\', '             \\\\          ')
-    while '  ' in content:
-        content = content.replace('  ', ' ')
-    while '{ ' in content:
-        content = content.replace('{ ', '{')
-    while ' }' in content:
-        content = content.replace(' }', '}')
-    with open(file_path, 'w', encoding="utf-8") as file:
-        file.write(content)
-        # 
-    with open(file_path, 'r', encoding="utf-8") as file:
-        contents = file.readlines()
-    contents = [line.strip() for line in contents]
-    with open(file_path, 'w', encoding="utf-8") as file:
-        for line in contents:
-            file.write(line + '\n')
-file_paths = glob.glob(os.path.join(latex_folder, f'**/*.md'), recursive=True)
-for file_path in file_paths:
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.read()
+#     content = content.replace('\\\\', '             \\\\          ')
+#     while '  ' in content:
+#         content = content.replace('  ', ' ')
+#     while '{ ' in content:
+#         content = content.replace('{ ', '{')
+#     while ' }' in content:
+#         content = content.replace(' }', '}')
+#     with open(file_path, 'w', encoding="utf-8") as file:
+#         file.write(content)
+#         # 
+#     with open(file_path, 'r', encoding="utf-8") as file:
+#         contents = file.readlines()
+#     contents = [line.strip() for line in contents]
+#     with open(file_path, 'w', encoding="utf-8") as file:
+#         for line in contents:
+#             file.write(line + '\n')
+# file_paths = glob.glob(os.path.join(latex_folder, f'**/*.md'), recursive=True)
+# for file_path in file_paths:
+#     with open(file_path, 'r', encoding="utf-8") as file:
+#         content = file.read()
 
-    content = content.replace('\\\\', '             \\\\          ')
-    while '  ' in content:
-        content = content.replace('  ', ' ')
-    while '{ ' in content:
-        content = content.replace('{ ', '{')
-    while ' }' in content:
-        content = content.replace(' }', '}')
-    with open(file_path, 'w', encoding="utf-8") as file:
-        file.write(content)
-        # 
-    with open(file_path, 'r', encoding="utf-8") as file:
-        contents = file.readlines()
-    contents = [line.strip() for line in contents]
-    with open(file_path, 'w', encoding="utf-8") as file:
-        for line in contents:
-            file.write(line + '\n')
+#     content = content.replace('\\\\', '             \\\\          ')
+#     while '  ' in content:
+#         content = content.replace('  ', ' ')
+#     while '{ ' in content:
+#         content = content.replace('{ ', '{')
+#     while ' }' in content:
+#         content = content.replace(' }', '}')
+#     with open(file_path, 'w', encoding="utf-8") as file:
+#         file.write(content)
+#         # 
+#     with open(file_path, 'r', encoding="utf-8") as file:
+#         contents = file.readlines()
+#     contents = [line.strip() for line in contents]
+#     with open(file_path, 'w', encoding="utf-8") as file:
+#         for line in contents:
+#             file.write(line + '\n')

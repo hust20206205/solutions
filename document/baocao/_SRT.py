69f468bbb5749227806028f5475e031c1a793f29
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-12 09:24:41 +0700

    2023_12_12_09_24_41_339766

--- document/baocao/_SRT.py
+++ document/baocao/_SRT.py
@@ -1,8 +1,8 @@
-file=r"C:\Users\vvn20206205\Documents\_______test\z\_DDD\08EventsDrivenArchitectureDomainEvents_VVN\000000001_vn2_nghia.mp4"
+# file=r"C:\Users\vvn20206205\Downloads\Video\VideoOK\_DDD"
 import os
 # print(os.path.dirname(file))
-folder=os.path.dirname(file)
-folder=r"C:\Users\vvn20206205\Downloads\_______test\z\_DDD"
+# folder=os.path.dirname(file)
+folder =r"C:\Users\vvn20206205\Downloads\Video\VideoOK\__DDD"
 import glob 
 def TimKiem(root_dir, ext):
         return glob.glob(os.path.join(

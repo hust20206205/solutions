d19ad43961131f02aa12f74244321a9eb74652f2
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-13 08:32:19 +0700

    2023_12_13_08_32_19_752374

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -1,24 +1,24 @@
+import os
+import glob
+from modules.MyClose import MyClose
+from modules.MyGit import MyGit
 from modules.MyImport import MyImport
 # MyImport.Import()
 from modules.MyNow import MyNow
 message = "VuVanNghia20206205"
 message = MyNow()
-from modules.MyGit import MyGit
 MyGit.add(message)
 MyGit.commit(message)
 # MyGit.push(message)
-from modules.MyClose import MyClose
 # MyClose.OpenGit()
 MyClose.ScrollBar()
 # MyClose.CollapseFolders()
 MyClose.CloseAll()
 MyClose.Target(2)
-# MyClose.Terminal()
+MyClose.Terminal()
 # from modules.MyChrome import MyChrome
 # MyChrome("https://github.com/vvn20206205/test")
 # MyChrome()
-import glob
-import os
 latex_folder = os.getcwd()
 file_paths = glob.glob(os.path.join(latex_folder, f'**/*.tex'), recursive=True)
 for file_path in file_paths:
@@ -34,91 +34,90 @@ for file_path in file_paths:
         '\\(': '$',
         '\\)': '$',
 
- 'Monolithic': 'kiến trúc nguyên khối',
- 'monolithic': 'kiến trúc nguyên khối',
+        'Monolithic': 'kiến trúc nguyên khối',
+        'monolithic': 'kiến trúc nguyên khối',
 
- 'Microservices': 'kiến trúc vi dịch vụ',
- 'microservices': 'kiến trúc vi dịch vụ',
- 'Microservice': 'kiến trúc vi dịch vụ',
- 'microservice': 'kiến trúc vi dịch vụ',
- 'dịch vụ vi mô': 'vi dịch vụ',
- 
- 'Domain-Driven Design': 'thiết kế hướng miền',
- 'Domain Driven Design': 'thiết kế hướng miền',
- 'domain driven design': 'thiết kế hướng miền',
- 'ddd': 'thiết kế hướng miền',
- 'DDD': 'thiết kế hướng miền',
- 
- 'patterns': 'mẫu',
- 'pattern': 'mẫu',
- 
- 'cơ sở dữ liệu': 'CSDL',
- 'csdl': 'CSDL',
- 'database': 'CSDL',
- 
- 'services': 'dịch vụ',
- 'service': 'dịch vụ',
- 
- 'đội': 'nhóm',
- 
- '![Alt text](': '![](',
- 
- 'Domain expert': 'chuyên gia ngành',
- 'domain expert': 'chuyên gia ngành',
- 'chuyên gia kinh doanh': 'chuyên gia ngành',
- 'chuyên gia nghiệp vụ': 'chuyên gia ngành',
- 'chuyên gia tên miền': 'chuyên gia ngành',
- 
- 'Ngôn ngữ phổ biến': 'ngôn ngữ chung',
- 'ngôn ngữ phổ biến': 'ngôn ngữ chung',
- 
- 'bạn': 'chúng ta',
- 'Bạn': 'Chúng ta',
- 
- 'tên miền': 'miền',
- 'Tên miền': 'Miền',
- 
- 'bị ràng buộc': 'giới hạn',
- 'bị giới hạn': 'giới hạn',
- 'bị chặn': 'giới hạn',
- 
- 'chống tham nhũng': 'chống đổ vỡ'
+        'Microservices': 'kiến trúc vi dịch vụ',
+        'microservices': 'kiến trúc vi dịch vụ',
+        'Microservice': 'kiến trúc vi dịch vụ',
+        'microservice': 'kiến trúc vi dịch vụ',
+        'dịch vụ vi mô': 'vi dịch vụ',
+
+        'Domain-Driven Design': 'thiết kế hướng miền',
+        'Domain Driven Design': 'thiết kế hướng miền',
+        'domain driven design': 'thiết kế hướng miền',
+        'ddd': 'thiết kế hướng miền',
+        'DDD': 'thiết kế hướng miền',
+
+        'patterns': 'mẫu',
+        'pattern': 'mẫu',
+
+        'cơ sở dữ liệu': 'CSDL',
+        'csdl': 'CSDL',
+        'database': 'CSDL',
+
+        'services': 'dịch vụ',
+        'service': 'dịch vụ',
+
+        'đội': 'nhóm',
+
+        '![Alt text](': '![](',
+
+        'Domain expert': 'chuyên gia ngành',
+        'domain expert': 'chuyên gia ngành',
+        'chuyên gia kinh doanh': 'chuyên gia ngành',
+        'chuyên gia nghiệp vụ': 'chuyên gia ngành',
+        'chuyên gia tên miền': 'chuyên gia ngành',
+
+        'Ngôn ngữ phổ biến': 'ngôn ngữ chung',
+        'ngôn ngữ phổ biến': 'ngôn ngữ chung',
+
+        'bạn': 'chúng ta',
+        'Bạn': 'Chúng ta',
+
+        'tên miền': 'miền',
+        'Tên miền': 'Miền',
+
+        'bị ràng buộc': 'giới hạn',
+        'bị giới hạn': 'giới hạn',
+        'bị chặn': 'giới hạn',
+
+        'chống tham nhũng': 'chống đổ vỡ'
     }
 
     for key, value in replacements.items():
         value = f"  {value}  "
         contents = contents.replace(key, value)
-     
-         
+
     while ' .' in contents:
-        contents = contents.replace(' .', '. ') 
-    contents = contents.replace(' .', '. ') 
+        contents = contents.replace(' .', '. ')
+    contents = contents.replace(' .', '. ')
     while ' ,' in contents:
-        contents = contents.replace(' ,', ', ') 
-    contents = contents.replace(' ,', ', ') 
+        contents = contents.replace(' ,', ', ')
+    contents = contents.replace(' ,', ', ')
     while ' ?' in contents:
-        contents = contents.replace(' ?', '? ') 
-    contents = contents.replace(' ?','? ') 
-    while ' :' in contents:
-        contents = contents.replace(' :', ': ') 
-    contents = contents.replace(' :',': ') 
-    while ' :' in contents:
-        contents = contents.replace(' :', ': ') 
-    contents = contents.replace(' :',': ') 
+        contents = contents.replace(' ?', '? ')
+    contents = contents.replace(' ?', '? ')
+    # while ' :' in contents:
+    #     contents = contents.replace(' :', ': ')
+    # contents = contents.replace(' :', ': ')
+    # while ' :' in contents:
+    #     contents = contents.replace(' :', ': ')
+    # contents = contents.replace(' :', ': ')
     while '  ' in contents:
         contents = contents.replace('  ', ' ')
     while '( ' in contents:
         contents = contents.replace('( ', '(')
     while ' )' in contents:
-        contents = contents.replace(' )', ')') 
+        contents = contents.replace(' )', ')')
     while '[ ' in contents:
         contents = contents.replace('[ ', '[')
     while ' ]' in contents:
-        contents = contents.replace(' ]', ']')  
+        contents = contents.replace(' ]', ']')
     while '{ ' in contents:
         contents = contents.replace('{ ', '{')
     while ' }' in contents:
-        contents = contents.replace(' }', '}') 
+        contents = contents.replace(' }', '}')
     contents = '\n'.join(line.strip() for line in contents.split('\n'))
     while "\n\n\n" in contents:
         contents = contents.replace("\n\n\n", "\n\n")
@@ -126,14 +125,10 @@ for file_path in file_paths:
         file.write(contents)
 
 
+#
+#
+#
+#
 
-
-
-
-# 
-# 
-# 
-# 
- 
 
 # # https://www.udemy.com/course/domain-driven-design-and-microservices

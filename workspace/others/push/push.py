e450258ee4b1f5739995706b10b1374b1306eba5
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-13 08:31:00 +0700

    2023_12_13_08_31_00_780954

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -32,7 +32,57 @@ for file_path in file_paths:
         '+': '+',
         '-': '-',
         '\\(': '$',
-        '\\)': '$'
+        '\\)': '$',
+
+ 'Monolithic': 'kiến trúc nguyên khối',
+ 'monolithic': 'kiến trúc nguyên khối',
+
+ 'Microservices': 'kiến trúc vi dịch vụ',
+ 'microservices': 'kiến trúc vi dịch vụ',
+ 'Microservice': 'kiến trúc vi dịch vụ',
+ 'microservice': 'kiến trúc vi dịch vụ',
+ 'dịch vụ vi mô': 'vi dịch vụ',
+ 
+ 'Domain-Driven Design': 'thiết kế hướng miền',
+ 'Domain Driven Design': 'thiết kế hướng miền',
+ 'domain driven design': 'thiết kế hướng miền',
+ 'ddd': 'thiết kế hướng miền',
+ 'DDD': 'thiết kế hướng miền',
+ 
+ 'patterns': 'mẫu',
+ 'pattern': 'mẫu',
+ 
+ 'cơ sở dữ liệu': 'CSDL',
+ 'csdl': 'CSDL',
+ 'database': 'CSDL',
+ 
+ 'services': 'dịch vụ',
+ 'service': 'dịch vụ',
+ 
+ 'đội': 'nhóm',
+ 
+ '![Alt text](': '![](',
+ 
+ 'Domain expert': 'chuyên gia ngành',
+ 'domain expert': 'chuyên gia ngành',
+ 'chuyên gia kinh doanh': 'chuyên gia ngành',
+ 'chuyên gia nghiệp vụ': 'chuyên gia ngành',
+ 'chuyên gia tên miền': 'chuyên gia ngành',
+ 
+ 'Ngôn ngữ phổ biến': 'ngôn ngữ chung',
+ 'ngôn ngữ phổ biến': 'ngôn ngữ chung',
+ 
+ 'bạn': 'chúng ta',
+ 'Bạn': 'Chúng ta',
+ 
+ 'tên miền': 'miền',
+ 'Tên miền': 'Miền',
+ 
+ 'bị ràng buộc': 'giới hạn',
+ 'bị giới hạn': 'giới hạn',
+ 'bị chặn': 'giới hạn',
+ 
+ 'chống tham nhũng': 'chống đổ vỡ'
     }
 
     for key, value in replacements.items():

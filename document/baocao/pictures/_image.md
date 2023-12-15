0a2bbcca7386911e6c33238856b20db8e312621b
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-15 20:13:56 +0700

    2023_12_15_20_13_56_616963

---
+++ document/baocao/pictures/_image.md
@@ -0,0 +1,10 @@
+========================================
+![](logoBK.png)
+
+\begin{figure}[h]
+\centering
+\includegraphics[height=3cm]{pictures\logoBK.png}
+\caption{vvn20206205}
+\label{pictures:logoBK.png}
+\end{figure}
+========================================
--- document/baocao/tools/image.py
+++ document/baocao/tools/image.py
@@ -10,10 +10,20 @@ def TimKiem(root_dir, ext):
 base_folder = os.path.join(os.getcwd(), "..")
 folder = os.path.join(base_folder, "pictures")
 
-print(folder)
 files = TimKiem(folder, '.png')
 files += TimKiem(folder, '.jpg')
 
-with open("_image.md", 'w') as markdown:
+with open(os.path.join(folder,"_image.md"), 'w') as markdown:
     for file in files:
-        markdown.write(f"![]({os.path.relpath(file, base_folder)})\n")
+        markdown.write(f"========================================\n")
+        markdown.write(f"![]({os.path.relpath(file, folder)})\n")
+        markdown.write(f"\n")
+        markdown.write(f"\\begin{{figure}}[h]\n")
+        markdown.write(f"\\centering\n")
+        markdown.write("\\includegraphics[height=3cm]{"+f"{os.path.relpath(file, base_folder)}"+"}\n")
+        markdown.write("\\caption{vvn20206205}\n")
+        markdown.write("\\label{pictures:"+ f"{os.path.relpath(file, folder)}"+"}\n")
+        markdown.write(f"\\end{{figure}}\n")
+        markdown.write(f"========================================\n")
+        
+ 

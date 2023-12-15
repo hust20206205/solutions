16d67fb56875ebca977e937d725ab43d1dac1470
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-15 18:16:50 +0700

    2023_12_15_18_16_50_108345

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -26,99 +26,100 @@ folder = os.getcwd()
 
 
 replacements = {
-        'Chuyển đến nội dung\nĐối với người hành nghề bởi người hành nghề\nTìm kiếm\nThiết kế hướng miền: Hướng dẫn dành cho người thực hành\nCâu hỏi thường gặp\nBảng chú giải\nVề chúng tôi\nCuốn sách của chúng tôi!': '',
+        '\n': '\n\n\n',
+        # 'Chuyển đến nội dung\nĐối với người hành nghề bởi người hành nghề\nTìm kiếm\nThiết kế hướng miền: Hướng dẫn dành cho người thực hành\nCâu hỏi thường gặp\nBảng chú giải\nVề chúng tôi\nCuốn sách của chúng tôi!': '',
 
 
 
-        'Thể loại\n\nPhân tích\nđiều cơ bản\nthiết kế hướng miền\nthiết kế\ncâu hỏi thường gặp\nKhả năng lãnh đạo\nhoa văn\nBlog tại WordPress.com.': '',
+        # 'Thể loại\n\nPhân tích\nđiều cơ bản\nthiết kế hướng miền\nthiết kế\ncâu hỏi thường gặp\nKhả năng lãnh đạo\nhoa văn\nBlog tại WordPress.com.': '',
 
 
-        'Thể loại\n\nPhân tích\nđiều cơ bản\nddd\nthiết kế\ncâu hỏi thường gặp\nKhả năng lãnh đạo\nhoa văn\nBlog tại WordPress.com.': '',
-        '\\\\': '\\\\',
-        '=': '=',
-        '+': '+',
-        # '#': '%@',
-        '<!--': '%!',
-        '-->': '',
-        '-': '-',
-        '\\(': '$',
-        '\\)': '$',
+        # 'Thể loại\n\nPhân tích\nđiều cơ bản\nddd\nthiết kế\ncâu hỏi thường gặp\nKhả năng lãnh đạo\nhoa văn\nBlog tại WordPress.com.': '',
+        # '\\\\': '\\\\',
+        # '=': '=',
+        # '+': '+',
+        # # '#': '%@',
+        # '<!--': '%!',
+        # '-->': '',
+        # '-': '-',
+        # '\\(': '$',
+        # '\\)': '$',
 
-        'Monolithic': 'kiến trúc nguyên khối',
-        'monolithic': 'kiến trúc nguyên khối',
+        # 'Monolithic': 'kiến trúc nguyên khối',
+        # 'monolithic': 'kiến trúc nguyên khối',
 
-        'Microservices': 'kiến trúc vi dịch vụ',
-        'microservices': 'kiến trúc vi dịch vụ',
-        'Microservice': 'kiến trúc vi dịch vụ',
-        'microservice': 'kiến trúc vi dịch vụ',
-        'dịch vụ vi mô': 'vi dịch vụ',
+        # 'Microservices': 'kiến trúc vi dịch vụ',
+        # 'microservices': 'kiến trúc vi dịch vụ',
+        # 'Microservice': 'kiến trúc vi dịch vụ',
+        # 'microservice': 'kiến trúc vi dịch vụ',
+        # 'dịch vụ vi mô': 'vi dịch vụ',
 
-        'Domain - Driven Design': 'thiết kế hướng miền',
-        'Domain-Driven Design': 'thiết kế hướng miền',
-        'Domain Driven Design': 'thiết kế hướng miền',
-        'domain driven design': 'thiết kế hướng miền',
-        'ddd': 'thiết kế hướng miền',
-        'DDD': 'thiết kế hướng miền',
+        # 'Domain - Driven Design': 'thiết kế hướng miền',
+        # 'Domain-Driven Design': 'thiết kế hướng miền',
+        # 'Domain Driven Design': 'thiết kế hướng miền',
+        # 'domain driven design': 'thiết kế hướng miền',
+        # 'ddd': 'thiết kế hướng miền',
+        # 'DDD': 'thiết kế hướng miền',
 
-        'patterns': 'mẫu',
-        'pattern': 'mẫu',
+        # 'patterns': 'mẫu',
+        # 'pattern': 'mẫu',
 
-        'cơ sở dữ liệu': 'CSDL',
-        'csdl': 'CSDL',
-        'database': 'CSDL',
+        # 'cơ sở dữ liệu': 'CSDL',
+        # 'csdl': 'CSDL',
+        # 'database': 'CSDL',
 
-        'services': 'dịch vụ',
-        'service': 'dịch vụ',
+        # 'services': 'dịch vụ',
+        # 'service': 'dịch vụ',
 
-        'đội': 'nhóm',
+        # 'đội': 'nhóm',
 
-        '![Alt text](': '![](',
+        # '![Alt text](': '![](',
 
-        'domain expert': 'chuyên gia ngành',
-        'chuyên gia kinh doanh': 'chuyên gia ngành',
-        'chuyên gia nghiệp vụ': 'chuyên gia ngành',
-        'chuyên gia tên miền': 'chuyên gia ngành',
-        'chuyên gia miền': 'chuyên gia ngành',
+        # 'domain expert': 'chuyên gia ngành',
+        # 'chuyên gia kinh doanh': 'chuyên gia ngành',
+        # 'chuyên gia nghiệp vụ': 'chuyên gia ngành',
+        # 'chuyên gia tên miền': 'chuyên gia ngành',
+        # 'chuyên gia miền': 'chuyên gia ngành',
 
-        'Domain expert': 'Chuyên gia ngành',
-        'Chuyên gia kinh doanh': 'Chuyên gia ngành',
-        'Chuyên gia nghiệp vụ': 'Chuyên gia ngành',
-        'Chuyên gia tên miền': 'Chuyên gia ngành',
-        'Chuyên gia miền': 'Chuyên gia ngành',
-
-
-
-        'Ngôn ngữ phổ biến': 'ngôn ngữ chung',
-        'ngôn ngữ phổ biến': 'ngôn ngữ chung',
-
-        'bạn': 'chúng ta',
-        'Bạn': 'Chúng ta',
-
-        'tên miền': 'miền',
-        'Tên miền': 'Miền',
-
-        'bị ràng buộc': 'giới hạn',
-        'bị giới hạn': 'giới hạn',
-        'bị chặn': 'giới hạn',
-
-        'chống tham nhũng': 'chống đổ vỡ',
-
-
-        'Tổng Cục Thuế': 'TCT',
-
-        'tổng Cục Thuế': 'TCT',
-        'Tổng cục Thuế': 'TCT',
-        'Tổng Cục thuế': 'TCT',
-
-        'tổng cục Thuế': 'TCT',
-        'tổng Cục thuế': 'TCT',
-        'Tổng cục thuế': 'TCT', 
+        # 'Domain expert': 'Chuyên gia ngành',
+        # 'Chuyên gia kinh doanh': 'Chuyên gia ngành',
+        # 'Chuyên gia nghiệp vụ': 'Chuyên gia ngành',
+        # 'Chuyên gia tên miền': 'Chuyên gia ngành',
+        # 'Chuyên gia miền': 'Chuyên gia ngành',
 
 
 
+        # 'Ngôn ngữ phổ biến': 'ngôn ngữ chung',
+        # 'ngôn ngữ phổ biến': 'ngôn ngữ chung',
 
-        'tổng cục thuế': 'TCT',
-        " ​​": '    ',
+        # 'bạn': 'chúng ta',
+        # 'Bạn': 'Chúng ta',
+
+        # 'tên miền': 'miền',
+        # 'Tên miền': 'Miền',
+
+        # 'bị ràng buộc': 'giới hạn',
+        # 'bị giới hạn': 'giới hạn',
+        # 'bị chặn': 'giới hạn',
+
+        # 'chống tham nhũng': 'chống đổ vỡ',
+
+
+        # 'Tổng Cục Thuế': 'TCT',
+
+        # 'tổng Cục Thuế': 'TCT',
+        # 'Tổng cục Thuế': 'TCT',
+        # 'Tổng Cục thuế': 'TCT',
+
+        # 'tổng cục Thuế': 'TCT',
+        # 'tổng Cục thuế': 'TCT',
+        # 'Tổng cục thuế': 'TCT', 
+
+
+
+
+        # 'tổng cục thuế': 'TCT',
+        # " ​​": '    ',
 
 
         
